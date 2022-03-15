e2d2be213ff885d49f020a867a61308a14941d1f
smmaleson <smaleson@princeton.edu>
2022-03-15 10:52:08 -0400

    index.html, database files, search method

--- registrarapp.py
+++ registrarapp.py
@@ -1,11 +1,30 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, make_response
+import reg_db
+import regdetails_db
 
 app = Flask(__name__, template_folder='.')
 
 
 @app.route('/', methods=['GET'])
 def base():
-    return render_template('index.html')
+
+    dept = request.args.get('dept')
+    print('dept:', dept)
+    num = request.args.get('number')
+    print('num:', num)
+    print(type(num))
+    area = request.args.get('area')
+    print('area:', area)
+    title = request.args.get('title')
+    print('title:', title)
+
+    courses = reg_db.db_access(dept, num, area, title)
+
+    html = render_template('index.html', courses=courses)
+    response = make_response(html)
+    # response.set_cookie('prev_author', author)
+
+    return response
 
 
 @app.route('/regdetails/', methods=['GET'])
